c4da5503e8a7288d550b46e68594afa583a3275d
Muchezz <muchezz@outlook.com>
2021-05-17 16:05:32 +0300

    User Registration

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author' : 'Mucheru',
-        'title' : 'Blog Post 1',
-        'content': 'First blog post',
-        'date_posted': 'May 14, 2021'
-    },
-    {
-        'author' : 'James',
-        'title' : 'Blog Post 2',
-        'content': 'Second blog post',
-        'date_posted': 'May 15, 2021'
-    }
-]
 
 # Create your views here.
 def home(request):
--- dproject/urls.py
+++ dproject/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from users import views as users_views #type: ignore
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', users_views.register, name = 'register'),
     path('', include('blog.urls')),
 ]
